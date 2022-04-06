034d022dfa698312dc654075b3725ef8927961c4
wudline <wudline.bf@gmail.com>
2022-04-06 17:51:13 +0200

    feat/	- Recherche component : search spell name

---
+++ src/components/Recherche.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="Recherche">
+    <p>Recherche</p>
+    <input type="text" v-model="search" placeholder="Spell name">
+    <ul id="SpellsList">
+      <li v-for="data in filteredSpells" :key="data.id">{{ data }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {SpellName} from '../assets/functions.js'
+
+export default {
+  name: 'Recherche',
+  data () {
+    return {
+      spellsList: SpellName(),
+      search: ''
+    }
+  },
+  computed: {
+    filteredSpells () {
+      return this.spellsList.filter((spell) => {
+        return spell.toLowerCase().match(this.search.toLowerCase())
+      })
+    }
+  }
+}
+</script>
