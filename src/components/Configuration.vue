d65f218e6b0e4b1256448eb38d9f200db31f5a5f
Louis Legrand <louislegrand42@gmail.com>
2022-04-07 19:23:38 +0200

    feat/ 	Le pendule de donné. 	Documentation.

--- src/components/Configuration.vue
+++ src/components/Configuration.vue
@@ -4,20 +4,42 @@
     <select-row list-name="Search Branches:" :data-list=branchesList />
     <select-row list-name="Search Classes & Domaines:" :data-list=classList />
     <select-row list-name="Search by niveau:" :data-list=spellLevelList />
+    <pendulum   list-name="Search by Livre:" />
+    <tr>
+      <td style="text-align: center" colspan="3">
+        <router-link to="/recherche">Recherche</router-link>
+      </td>
+    </tr>
   </table>
 </template>
 
 <script>
 import {SpellSchool, SpellClass, SpellLevel, SpellBranches} from '../assets/functions.js'
 import SelectRow from './configuration/SelectRow'
+import Pendulum from './configuration/Pendulum'
+
+/**
+ * Main component for the configuration page. It contains a table with all the option of to filter the spells.
+ * The list of option are :
+ *  - Spell school.
+ *  - Spell class.
+ *  - Spell level.
+ *  - Spell Branches.
+ *  - Spell book.
+ * These filters are then stored to be used by the research page.
+ * */
 export default {
   name: 'Configuration',
-  components: {SelectRow},
+  components: {Pendulum, SelectRow},
   data () {
     return {
+      /* List of school. */
       schoolList: SpellSchool(),
+      /* List of class. */
       classList: SpellClass(),
+      /* List of spell level. */
       spellLevelList: SpellLevel(),
+      /* List of branches. */
       branchesList: SpellBranches()
     }
   }
--- src/components/configuration/SelectRow.vue
+++ src/components/configuration/SelectRow.vue
@@ -14,6 +14,16 @@
 
 <script>
 import ParamToggleSwitch from './ParamToggleSwitch'
+
+/**
+ * This component represents a selection row that can be disabled by a subcomponent named 'param-toggle-switch'.
+ * Is passed by properties to this component:
+ *  - listName, a name to be display and to identify the row.
+ *  - dataList, a list of data from which the user can do his selection.
+ *  The data of this component:
+ *   - isActive, is a data which represent the availability of this component. It is controlled by an event of the
+ *  subcomponent 'param-toggle-switch'.
+ */
 export default {
   name: 'SelectRow',
   components: {ParamToggleSwitch},
