bd68dff936241d25418cb26cae047b5a65444fdc
wudline <wudline.bf@gmail.com>
2022-04-11 14:07:45 +0200

    feat/	SpellsNumber.vue component to compute statistics 	Statistiques.vue to print results : import SpellsNumber component

---
+++ src/components/Statistiques.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-link to="/">Configuration</router-link>
+    <router-link to="/Recherche">Recherche</router-link>
+    <p>Statistiques</p>
+    <spells-number :spells-array=dataTable :spells-book-title=booksTable />
+  </div>
+</template>
+
+<script>
+import SpellsNumber from './statistiques/SpellsNumber'
+import {sortTable} from '../assets/data.min'
+import {SpellBookTitle} from '../assets/functions'
+
+export default {
+  name: 'Statistiques',
+  components: {SpellsNumber},
+  data () {
+    return {
+      dataTable: sortTable,
+      booksTable: SpellBookTitle()
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
---
+++ src/components/statistiques/SpellsNumber.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>There are {{ spellsBookTitle.length }} books referenced</p>
+    <p>There are {{ spellsArray.length }} spells referenced</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpellsNumber',
+  props: ['spellsArray', 'spellsBookTitle'],
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
